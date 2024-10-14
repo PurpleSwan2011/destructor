3e20e6822d62d814400020a93358f51b5bc952aa
Saima Fathima <saima.fathi2011@gmail.com>
2024-10-14 20:26:39 +0530

    Add files via upload

---
+++ destructors.py
@@ -0,0 +1,10 @@
+class employee :
+    def __int__(self):
+        print("employee created")
+    def __del__(self):
+        print("destructor called employ delete")
+obj = employee()
+del obj 
+
+    
+
